cb857fb832b0b0169053810f2b3aa13409ad48e6
rcontini <rachelecontini5@gmail.com>
2022-05-24 14:42:02 -0400

    trying to display the forecast

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function displayForcast(response) {
   let forcastElement = document.querySelector("#forcast");
   let days = ["Thu", "Fri", "Sat", "Sun"];
   days.forEach(function (day) {
-    let forcastHTML = `<div class="card-body" ><div class="row">`;
+    let forcastHTML = `<div class="row">`;
 
     forcastHTMl =
       forcastHTML +
@@ -41,14 +41,14 @@ function displayForcast(response) {
                 >
               </div>`;
 
-    forcastHTML = forcastHTML + `</div></div>`;
+    forcastHTML = forcastHTML + `</div>`;
     forcastElement.innerHTML = forcastHTML;
   });
 }
 
 function getForcast(coordinates) {
   let apiKey = "4de5d6165fffbb5d356ef70ff72b3431";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayForcast);
 }
