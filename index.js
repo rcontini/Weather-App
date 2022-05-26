614660bb01d11f033eee138b416e5555003d0f58
rcontini <rachelecontini5@gmail.com>
2022-05-25 20:05:41 -0400

    removed windspeed

--- index.js
+++ index.js
@@ -80,9 +80,7 @@ function displayWeather(response) {
   document.querySelector("#currentTemp").innerHTML =
     Math.round(celsiusTemperature);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#windspeed").innerHTML = Math.round(
-    response.data.wind.speed
-  );
+
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
   iconElement.setAttribute(
